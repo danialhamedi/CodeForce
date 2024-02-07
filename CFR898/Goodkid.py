7084cb4d6e481701153fd66220c49ece5a8a0252
Danial Hamedi <danial@datachef.co>
2024-02-08 01:37:58 +0330

    goodkid solution

--- CFR898/Goodkid.py
+++ CFR898/Goodkid.py
@@ -1,4 +1,10 @@
-def solution():
-	pass
+num_tests = int(input())
 
-solution(i)
+for _ in range(num_tests):
+    input()  # Discard the first input, assuming it's not used
+    arr = sorted(map(int, input().split()))
+    arr[0] += 1
+    result = 1
+    for num in arr:
+        result *= num
+    print(result)
